69c2e8da85a4e67b6f2660d97b2075f0023c66f4
pythonina <pythonina@gmail.com>
2023-01-11 13:11:09 +0330

    fix bugs

--- src/pages/createads/CreateAds.js
+++ src/pages/createads/CreateAds.js
@@ -343,7 +343,7 @@ const CreateAds = () => {
                             size="medium"
                         />
                         <Button sx={{ mt: 4, backgroundColor: '#FF5959', width: '70%' }} variant='contained' onClick={submitForm}>
-                            {adsSubmitClicked === true ? <CircularProgress /> : 'ارسال آگهی'}
+                            {adsSubmitClicked === true ? <CircularProgress size={'36.46px'} /> : 'ارسال آگهی'}
                         </Button>
                     </>
                 }
